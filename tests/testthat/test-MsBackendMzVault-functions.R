227ed967bbf58c8eed799d14d5e3eca32a9bb8e5
Michele Stravs <stravsmi@eawag.ch>
2023-01-25 15:48:52 +0100

    completed testing load_spectravariables_mapping

--- tests/testthat/test-MsBackendMzVault-functions.R
+++ tests/testthat/test-MsBackendMzVault-functions.R
@@ -40,14 +40,64 @@ test_that("Processing the column mapping works",  {
     object = NA,
     mapping = column_map)
 
-  # check the full_specification variant
+  ### check the full_specification variant
+  # col
   expect_equal(
     processed_column_map$full_specification$col,
     "inputcol"
   )
+  # read_fun
   expect_equal(
     processed_column_map$full_specification$read_fun(c(1,2,3)),
     c(2,3,4)
+  )
+
+  ### check the char_for_col variant
+  # col
+  expect_equal(
+    processed_column_map$char_for_col$col,
+    "inputcol"
+  )
+  # read_fun
+  expect_equal(
+    processed_column_map$char_for_col$read_fun(c(1,2,3)),
+    c(1,2,3)
+  )
+
+  ### check the constant(v) variant
+  # col
+  expect_equal(
+    processed_column_map$using_constant$col,
+    "acquisitionNum"
+  )
+  # read_fun
+  expect_equal(
+    processed_column_map$using_constant$read_fun(c(22,33,11)),
+    c(55,55,55)
+  )
+
+  ### check the function_along_acquisitionNum variant
+  # col
+  expect_equal(
+    processed_column_map$function_along_acquisitionNum$col,
+    "acquisitionNum"
+  )
+  # read_fun
+  expect_equal(
+    processed_column_map$function_along_acquisitionNum$read_fun(c(22,33,11)),
+    c(21,32,10)
+  )
+
+  ### check the NA variant
+  # col
+  expect_equal(
+    processed_column_map$constant_NA$col,
+    "acquisitionNum"
+  )
+  # read_fun
+  expect_equal(
+    processed_column_map$constant_NA$read_fun(c(22,33,11,44)),
+    c(NA, NA, NA, NA)
   )
 
 })
